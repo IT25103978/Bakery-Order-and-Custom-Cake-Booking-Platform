076a0a115dbe6a48892b2576079bcdb3b1418f8c
Sahas-Pathirana <sahaspathirana2006@gmail.com>
2026-04-13 23:25:07 +0530

    customers file added

---
+++ backend/src/main/java/com/bakery/model/customers.java
@@ -0,0 +1,18 @@
+
+
+@Entity
+@Table(name = "customers")
+@Data
+@NoArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String address;
+    private String password;
+}
+
