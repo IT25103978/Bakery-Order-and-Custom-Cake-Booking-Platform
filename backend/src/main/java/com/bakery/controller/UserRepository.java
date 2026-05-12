dec75f7d89709f09d0d2cd5823dd0ffab045fae5
IT25100218 <it25100218@my.sliit.lk>
2026-05-12 14:24:11 +0530

    Add files via upload

---
+++ backend/src/main/java/com/bakery/controller/UserRepository.java
@@ -0,0 +1,8 @@
+package com.bakery.weblogin;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsernameAndPassword(String username, String password);
+    User findByUsername(String username);
+}
---
+++ backend/src/main/java/com/bakery/controller/WebloginApplication.java
@@ -0,0 +1,13 @@
+package com.bakery.weblogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebloginApplication.class, args);
+	}
+//http://localhost:8080/login
+}
