a4a6487098e7c6bf422ada61e366f07e3599afaa
IT25103325 <IT25103325@my.sliit.lk>
2026-05-15 13:08:10 +0530

    updated project structure

--- backend/src/main/java/com/bakery/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bakery.weblogin;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByUsernameAndPassword(String username, String password);
-    User findByUsername(String username);
-}
